81c5103adfe88afa8b9f02d9498240f3a67eff43
Habtamu Petros <89646714+habte0904@users.noreply.github.com>
2023-10-04 08:52:13 +0300

    removed www.busbud.com

--- E2E.py
+++ E2E.py
@@ -1,9 +1,7 @@
-import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
 
 driver = webdriver.Chrome()
-driver.get("https://www.busbud.com/")
+driver.get("https://www.facebook.com")
 driver.maximize_window()

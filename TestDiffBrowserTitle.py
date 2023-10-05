a3b61ce22969e28080575dbcf8c55d494fdd7bfd
Habtamu Petros <89646714+habte0904@users.noreply.github.com>
2023-10-05 04:49:38 +0300

    second unittest

--- TestDiffBrowserTitle.py
+++ TestDiffBrowserTitle.py
@@ -4,14 +4,20 @@ from selenium import webdriver
 
 class BrowserTitle(unittest.TestCase):
 
-    def googleTitle(self):
+    def test_googleTitle(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.google.com")
         print(self.driver.title)
         self.driver.close()
 
-    def rahelShetTitle(self):
+    def test_rahelShetTitle(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+        print(self.driver.title)
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
